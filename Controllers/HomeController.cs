bffea164c0af10c325dc3abaf7ca8df6faebb0bb
Rafał Kwiecień <53783787+Arcanagos@users.noreply.github.com>
2022-01-13 21:39:07 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Result(Model model)
+        {
+            var dateBirthday = DateTime.Parse(model.BirthDate);
+            model.Days = (DateTime.Now - dateBirthday).Days.ToString();
+            model.Leap = DateTime.IsLeapYear(dateBirthday.Year);
+            return View("Result",model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2.Models
+{
+    public class Model
+    {
+        [Display(Name = "Imie")]
+        public string Name { get; set; }
+        [Display(Name = "Liczba dni od dnia urodzenia")]
+        public string Days { get; set; }
+        [Display(Name = "Liczba dni od dnia urodzenia")]
+        public string BirthDate { get; set; }
+        [Display(Name = "Rok przestępny?")]
+        public bool Leap { get; set; }
+    }
+}
